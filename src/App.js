96352448f338782291214774ea61340488de388e
Abhilash Rawal <abhilashrawal091@gmail.com>
2023-04-03 13:01:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TaskList from './pages/TaskList';
+import Profile from './pages/Profile';
+import CreateTask from './pages/CreateTask';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter >
+    <Navigation />
+    <Routes>
+   <Route path='/' element={<Home />}>  </Route>
+   <Route path='/about' element={<About />}>  </Route>
+   <Route path='/task-list' aboutelement={<TaskList />}>  </Route>
+   <Route path='/profile' element={<Profile />}>  </Route>
+   <Route path='/create-task' element={<CreateTask />}>  </Route>
+
+
+ 
+    </Routes>
+    </BrowserRouter>
+   
+  )
 }
 
 export default App;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile(props) {
+    return (
+        <div>
+           <h1 >hello this is profile side</h1>
+        </div>
+    );
+}
+
+export default Profile;
