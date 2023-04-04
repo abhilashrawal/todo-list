be5cdf04b00f81538b23c2afe1bebf257819dc84
Abhilash Rawal <abhilashrawal091@gmail.com>
2023-04-04 13:37:03 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes ,Navigate} from 'react-router-dom';
 import TaskList from './pages/TaskList';
 import Profile from './pages/Profile';
 import CreateTask from './pages/CreateTask';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navigation from './components/Navigation';
+import PageNotFound from './pages/PageNotFound';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <BrowserRouter >
     <Navigation />
     <Routes>
-   <Route path='/' element={<Home />}>  </Route>
+    <Route path="/" element={<Navigate to="/login"/>}></Route>
+   <Route path='/' element={<Home />}>
+    <Route path='/login' element={<Login />}></Route>
+    <Route path='/register' element={<Register />}>
+    </Route> 
+    
+    
+    </Route>
+
    <Route path='/about' element={<About />}>  </Route>
    <Route path='/task-list' aboutelement={<TaskList />}>  </Route>
    <Route path='/profile' element={<Profile />}>  </Route>
    <Route path='/create-task' element={<CreateTask />}>  </Route>
+   <Route path='*' element={<PageNotFound />}> </Route>
 
 
  
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageNotFound(props) {
+    return (
+        <div className='text-cem=nter py-5'>
+         <h1 className='dispaly-1'  >   Error 404 :page not found</h1>  
+        </div>
+    );
+}
+
+export default PageNotFound;
